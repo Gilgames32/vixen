6824221cc9a4e7b62c8698adeee0eb2c2f5c66d8
Bálint Kovács <balintkovacs11@gmail.com>
2025-12-21 16:48:00 +0100

    biome dependent skins

--- skins.lua
+++ skins.lua
@@ -1,9 +1,14 @@
 local wheel = require("wheel")
 
+-- automatic skin updated based on the biome
+local auto = false
+local skinUpdateInterval = 80
+local skinUpdateTimer = 0
+
 local skinEntries = {
     Rebecca = {
         texture = "skin_rebecca",
-        icon = "minecraft:command_block",
+        icon = "minecraft:redstone",
     },
     Mira = {
         texture = "skin_mira",
@@ -23,12 +28,68 @@ local skinEntries = {
     },
 }
 
+local function setSkin(newTexture)
+    models.model.root:setPrimaryTexture("CUSTOM", textures[newTexture])
+end
+
 -- skin entries
 for key, value in pairs(skinEntries) do
     wheel.skinPage:newAction()
     :setTitle(key)
     :setItem(value.icon)
     :onLeftClick(function (_)
-        models.model.root:setPrimaryTexture("CUSTOM", textures[value.texture])
+        auto = false
+        setSkin(value.texture)
     end)
+end
+wheel.skinPage:newAction()
+:setTitle("auto")
+:setItem("minecraft:command_block")
+:onLeftClick(function (_)
+    auto = true
+    skinUpdateTimer = skinUpdateInterval
+end)
+
+
+local function autoSkin()
+    -- eva in nether
+    local dimension = world.getDimension()
+    if dimension == "minecraft:the_nether" then
+        setSkin(skinEntries.Eva.texture)
+        return
+    end
+    -- mira in end
+    if dimension == "minecraft:the_end" then
+        setSkin(skinEntries.Mira.texture)
+        return
+    end
+
+    local biome = world.getBiome(player:getPos())
+    local temperature = biome:getTemperature()
+    
+    -- kaya in cold 
+    if temperature <= 0.25 then
+        setSkin(skinEntries.Kaya.texture)
+        return
+    end
+
+    -- eva in dark forests
+    if biome.id == "minecraft:dark_forest" then
+        setSkin(skinEntries.Eva.texture)
+        return
+    end
+
+    setSkin(skinEntries.Rue.texture)
+end
+
+function events.tick()
+    if not auto then return end
+    if skinUpdateTimer < skinUpdateInterval then
+        skinUpdateTimer = skinUpdateTimer + 1
+        return
+    else
+        skinUpdateTimer = 0
+    end
+
+    autoSkin()
 end
