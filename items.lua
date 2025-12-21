bba0167a531cbd8057a8d98c9ba5ddb18f9a0d04
Bálint Kovács <balintkovacs11@gmail.com>
2025-12-21 17:24:05 +0100

    block holding proof of concept

--- items.lua
+++ items.lua
@@ -13,10 +13,15 @@ local tools = {
     "minecraft:wooden_sword",
 }
 
+local blocks = {
+    --"minecraft:grass_block",
+}
+
 local mouthIP = models.model.root.Head.MouthIPivot
 local mouthTP = models.model.root.Head.MouthTPivot
 local rightIP = models.model.root.RightArm.RightItemPivot
 local leftIP = models.model.root.LeftArm.LeftItemPivot
+local enderIP = models.model.root.EnderIPivot
 
 function events.tick()
     local handedness = false -- TODO player:isLeftHanded()
@@ -24,9 +29,17 @@ function events.tick()
     
     local isItem = contains(items, mainHandItem.id)
     local isTool = contains(tools, mainHandItem.id)
+    local isBlock = contains(blocks, mainHandItem.id)
     local isSprinting = player:isSprinting()
+
+    if isBlock then
+        animations.model.blockhold:play()
+    else
+        animations.model.blockhold:stop()
+    end
+    enderIP:setParentType((isBlock and not isSprinting) and "RightItemPivot" or "None")
     
-    rightIP:setParentType((not handedness and not isItem and not (isTool and isSprinting)) and "RightItemPivot" or "None")
+    rightIP:setParentType((not handedness and not isItem and not isBlock and not (isTool and isSprinting)) and "RightItemPivot" or "None")
     -- TODO fix pivot breaking when the secondary arms parent type is changed 
     --leftIP:setParentType((handedness and not isItem and not isTool) and "LeftItemPivot" or "None")
     
